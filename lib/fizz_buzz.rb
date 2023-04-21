bc2520eff38e96b02defdb9731a0599a51c7cd64
kei-prog <kei08prog@gmail.com>
2023-04-21 11:40:03 +0000

    complete!

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,14 @@
+require 'debug'
+
+def fizz_buzz(n)
+  binding.break
+  if n % 15 == 0
+    'FizzBuzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+end
